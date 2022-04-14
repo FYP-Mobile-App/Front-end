3151737ee5dd91ce621aea26e9b83da7bac0dc7c
marniekassar <marnie.kassar@hotmail.co.uk>
2022-04-14 18:52:06 +0300

    first iteration

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -1,38 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import '../../App.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
+let Web3 = require("web3");
+let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 export default function SignUpPage() {
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Join us</h2>
+      <h5>Create your personal account</h5>
+      <form action="/home">
+        <p>
+          <label>Phone number</label>
+          <br />
+          <input type="text" name="phone_number" required />
+        </p>
+        <p>
+          <label>Email address</label>
+          <br />
+          <input type="email" name="email" required />
+        </p>
+        <p>
+          <label>Password</label>
+          <br />
+          <input type="password" name="password" requiredc />
+        </p>
+        <p>
+          <button onClick={createAccount} id="sub_btn" type="submit">
+            Register
+          </button>
+        </p>
+      </form>
+      <footer>
+        <p>
+          <Link to="/">Back to Homepage</Link>
+        </p>
+      </footer>
+    </div>
+  );
+}
 
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Join us</h2>
-            <h5>Create your personal account</h5>
-            <form action="/home">
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required />
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" requiredc />
-                </p>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
-    )
-
+function createAccount() {
+  let account = web3.eth.accounts.create();
+  console.log(account);
 }

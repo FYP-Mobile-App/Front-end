70b6043792900730f37a5e8b9bf96b814ad4bcf8
marniekassar <marnie.kassar@hotmail.co.uk>
2022-04-19 14:42:54 +0300

    connected the frontend to the backend

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,40 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import "../../App.css";
 
-export default function SignInPage() {
-  return (
-    <div className="text-center m-5-auto">
-      <h2>Sign in</h2>
-      <form action="/home">
-        <p>
-          <label>Phone number</label>
-          <br />
-          <input type="text" name="phone_number" required />
-        </p>
-        <p>
-          <label>Password</label>
-          <Link to="/forget-password">
-            <label className="right-label">Forget password?</label>
-          </Link>
-          <br />
-          <input type="password" name="password" required />
-        </p>
-        <p>
-          <button id="sub_btn" type="submit">
-            Login
-          </button>
-        </p>
-      </form>
-      <footer>
-        <p>
-          First time? <Link to="/register">Create an account</Link>
-        </p>
-        <p>
-          <Link to="/">Back to Homepage</Link>
-        </p>
-      </footer>
-    </div>
-  );
+export default class SignInPage extends React.Component {
+  state = {
+    phone: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      phone: event.target.value,
+      password: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      phone: this.state.phone,
+      password: this.state.password,
+    };
+
+    axios.post(`http://localhost:9900/auth/login`, user).then((res) => {
+      console.log(res);
+      console.log(res.data);
+    });
+  };
+
+  render() {
+    return (
+      <div className="text-center m-5-auto">
+        <h2>Sign in</h2>
+        <form onSubmit={this.handleSubmit} action="/home">
+          <p>
+            <label>Phone number</label>
+            <br />
+            <input
+              type="text"
+              name="phone"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label>Password</label>
+            <Link to="/forget-password">
+              <label className="right-label">Forget password?</label>
+            </Link>
+            <br />
+            <input
+              type="password"
+              name="password"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <button id="sub_btn" type="submit">
+              Login
+            </button>
+          </p>
+        </form>
+        <footer>
+          <p>
+            First time? <Link to="/register">Create an account</Link>
+          </p>
+          <p>
+            <Link to="/">Back to Homepage</Link>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -1,43 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "../../App.css";
 let Web3 = require("web3");
 let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
-export default function SignUpPage() {
-  return (
-    <div className="text-center m-5-auto">
-      <h2>Join us</h2>
-      <h5>Create your personal account</h5>
-      <form action="/home">
-        <p>
-          <label>Phone number</label>
-          <br />
-          <input type="text" name="phone_number" required />
-        </p>
-        <p>
-          <label>Email address</label>
-          <br />
-          <input type="email" name="email" required />
-        </p>
-        <p>
-          <label>Password</label>
-          <br />
-          <input type="password" name="password" requiredc />
-        </p>
-        <p>
-          <button onClick={createAccount} id="sub_btn" type="submit">
-            Register
-          </button>
-        </p>
-      </form>
-      <footer>
-        <p>
-          <Link to="/">Back to Homepage</Link>
-        </p>
-      </footer>
-    </div>
-  );
+export default class SignUpPage extends React.Component {
+  state = {
+    phone: "",
+    password: "",
+    publickey: "",
+    privatekey: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      phone: event.target.value,
+      password: event.target.value,
+      publickey: "fyp",
+      privatekey: "cadmos",
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      phone: this.state.phone,
+      password: this.state.password,
+      publickey: this.state.publickey,
+      privatekey: this.state.privatekey,
+    };
+
+    axios
+      .post(`http://localhost:9900/auth/signup`, user)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    return (
+      <div className="text-center m-5-auto">
+        <h2>Join us</h2>
+        <h5>Create your personal account</h5>
+        <form onSubmit={this.handleSubmit} action="/home">
+          <p>
+            <label>Phone number</label>
+            <br />
+            <input
+              type="text"
+              name="phone"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <button onClick={createAccount} id="sub_btn" type="submit">
+              Register
+            </button>
+          </p>
+        </form>
+        <footer>
+          <p>
+            <Link to="/">Back to Homepage</Link>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 function createAccount() {

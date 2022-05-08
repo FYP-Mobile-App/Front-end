ca2d52fe9e0d974cfa196b04b0948a54da8f043e
rimchidiac <rim.chidiac@net.usj.edu.lb>
2022-05-08 22:34:08 +0300

    UI/UX changes

---
+++ src/components/nav/RightNav.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link, NavLink} from "react-router-dom";
+
+const Ul = styled.ul `
+    list-style: none;
+    display: flex;
+    flex-flow: row nowrap;
+
+    li{
+        padding: 18px 10px;
+    }
+
+    @media (max-width: 768px) {
+        flex-flow: column nowrap;
+        background-color: #0D2538;
+        position: fixed;
+        transform:  ${({open}) => open ? 'translateX(0)' : 'translateX(100%)'};
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 300px;
+        padding-top: 3.5rem;
+        transition: transform 0.3s ease-in-out;
+
+        li{
+            color: #fff;
+        }
+    }
+`;
+
+const RightNav = ({open}) => {
+  return (
+    <Ul open= {open}>
+        <li ><NavLink exact to="/home" activeClassName='active-link'>
+            Home
+        </NavLink></li>
+        <li>Settings</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li><NavLink exact to="/">
+            Log out
+        </NavLink></li>
+    </Ul>
+  )
+}
+
+export default RightNav

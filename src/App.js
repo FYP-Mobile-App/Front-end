0d511073d2ad77d9922900e61cfb3ee035cfe0e6
rimchidiac <rim.chidiac@net.usj.edu.lb>
2022-05-08 23:11:20 +0300

    night mode button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import LandingPage from "./components/pages/LandingPage";
 import LoginPage from "./components/pages/LoginPage";
 import PhoneNumberPage from "./components/pages/PhoneNumberPage";
@@ -9,24 +9,66 @@ import RegisterPage from "./components/pages/RegisterPage";
 import ForgetPasswordPage from "./components/pages/ForgetPasswordPage";
 import HomePage from "./components/pages/HomePage";
 import SendTransactionPage from "./components/pages/SendTransactionPage";
+import TransactionTypePage from "./components/pages/TransactionTypePage";
+import storage from "local-storage-fallback";
 
 import "./App.css";
 
+const GlobalStyle = createGlobalStyle`
+body{
+  background-color: ${props => 
+    props.theme.mode === 'dark' ? '#111' : '#EEE'};
+  color: ${props => 
+    props.theme.mode === 'dark' ? '#EEE' : '#111'};
+}
+`;
+
+function getInitialTheme() {
+  const savedTheme = storage.getItem('theme');
+  return savedTheme ? JSON.parse(savedTheme) : { mode: 'light' };
+}
+
 export default function App() {
+  const [theme, setTheme] =useState(getInitialTheme);
+  useEffect(
+    () => {
+      storage.setItem('theme', JSON.stringify(theme));
+    },
+    [theme]
+  );
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/phone-number" component={PhoneNumberPage} />
-          <Route path="/otp" component={OTPPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/forget-password" component={ForgetPasswordPage} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/send-transaction" component={SendTransactionPage} />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <>
+      <GlobalStyle />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/phone-number" component={PhoneNumberPage} />
+              <Route path="/otp" component={OTPPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/forget-password" component={ForgetPasswordPage} />
+              <Route path="/home" component={HomePage} />
+              <Route path="/send-transaction" component={SendTransactionPage} />
+              <Route path="/transaction-type" component={TransactionTypePage} />
+            </Switch>
+          </div>
+        </Router>
+          <input type="checkbox" class="checkbox" id="checkbox" />
+          <label for="checkbox" class="label" onClick={e =>
+            setTheme(
+              theme.mode === 'dark' 
+                ? {mode:'light'}
+                :{mode:'dark'}
+              )
+            }>
+            <i class="fas fa-moon"></i>
+            <i class='fas fa-sun'></i>
+          <div class='ball' />
+          </label>
+      </>
+    </ThemeProvider>
   );
 }
